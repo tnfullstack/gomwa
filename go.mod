59881187ab4c1ba274cc2d05551b8c72185e32f9
Thanh Nguyen <chrisng9@outlook.com>
2022-01-09 17:37:03 -0800

    Added mux.NewServer code

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tvn9/gomwa
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- web1/pkg/handlers/handlers.go
+++ web1/pkg/handlers/handlers.go
@@ -1,32 +1,39 @@
 package handlers
 
 import (
-	"net/http"
-	"text/template"
+    "net/http"
+    "text/template"
 )
 
+// declair tmpl object
 var tmpl *template.Template
 
+// init initialize the template variable
 func init() {
-	tmpl = template.Must(template.ParseGlob("./templates/*.tmpl"))
+    tmpl = template.Must(template.ParseGlob("../templates/*.tmpl"))
 }
 
 // Home returns the web app home page
 func Home(w http.ResponseWriter, r *http.Request) {
-	tmpl.ExecuteTemplate(w, "index.tmpl", nil)
+    tmpl.ExecuteTemplate(w, "index.tmpl", nil)
 }
 
 // About returns the web app about page
 func About(w http.ResponseWriter, r *http.Request) {
-	tmpl.ExecuteTemplate(w, "about.tmpl", nil)
+    tmpl.ExecuteTemplate(w, "about.tmpl", nil)
 }
 
-// About returns the web app about page
+// Login returns the web app about page
 func Login(w http.ResponseWriter, r *http.Request) {
-	tmpl.ExecuteTemplate(w, "login.tmpl", nil)
+    tmpl.ExecuteTemplate(w, "login.tmpl", nil)
 }
 
-// About returns the web app about page
+// Signup returns the web app about page
 func Signup(w http.ResponseWriter, r *http.Request) {
-	tmpl.ExecuteTemplate(w, "signup.tmpl", nil)
+    tmpl.ExecuteTemplate(w, "signup.tmpl", nil)
+}
+
+// Contacts returns the web app about page
+func Contacts(w http.ResponseWriter, r *http.Request) {
+    tmpl.ExecuteTemplate(w, "contacts.tmpl", nil)
 }
