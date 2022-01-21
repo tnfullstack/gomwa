1128762fd209c313320a925bdaa5396e0d50cfe3
Thanh Nguyen <chrisng9@outlook.com>
2022-01-21 06:10:12 -0800

    Added template and templates caches function

--- web3/render/render.go
+++ web3/render/render.go
@@ -1,17 +1,69 @@
 package render
 
 import (
+	"bytes"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
+
+// functions
+var functions = template.FuncMap{}
 
 // renderTemplates
 func RenderTemplates(w http.ResponseWriter, str string) {
-	tmpl, _ := template.ParseFiles("templates/" + str)
-
-	err := tmpl.Execute(w, nil)
+	tc, err := CreateTmplCache()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	t, ok := tc[str]
+	if !ok {
+		log.Fatal(err)
+	}
+
+	buf := new(bytes.Buffer)
+
+	_ = t.Execute(buf, nil)
+
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// CreateTmplCache
+func CreateTmplCache() (map[string]*template.Template, error) {
+	tmplCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.html")
+	if err != nil {
+		return tmplCache, err
+	}
+
+	for _, p := range pages {
+		name := filepath.Base(p)
+		fmt.Println(p)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(p)
+		if err != nil {
+			return tmplCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.html")
+		if err != nil {
+			return tmplCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.html")
+			if err != nil {
+				return tmplCache, err
+			}
+		}
+
+		tmplCache[name] = ts
+	}
+	return tmplCache, nil
 }
